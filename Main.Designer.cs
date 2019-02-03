a983fdac8e9d2c6c330c17a69123f5b60d5dd72c
yTsunomoto <wasiwasimaru@gmail.com>
2019-02-03 17:24:59 +0900

    update

---
+++ Main.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace MultiLangSample
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("游ゴシック", 28F);
+            this.label1.Location = new System.Drawing.Point(88, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(442, 97);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hello World";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("游ゴシック", 12F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "日本語",
+            "英語"});
+            this.comboBox1.Location = new System.Drawing.Point(51, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(298, 50);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,41 @@
+﻿using MultiLangSample.Properties;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace MultiLangSample
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+            var locales = new KeyValuePair<string, string>[]
+            {
+                new KeyValuePair<string, string>("ja-JP", "日本語"),
+                new KeyValuePair<string, string>("en-US", "英語")
+            };
+
+            // コンボボックスに KeyValuePair をバインドする
+            comboBox1.DataSource = locales;
+            comboBox1.DisplayMember = "Value";
+            comboBox1.ValueMember = "Key";
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            // リソースファイルからラベル文字列を取得
+            label1.Text = Resources.Greeting;
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // リソースファイルのカルチャーを変更する
+            var selected = (KeyValuePair<string, string>)comboBox1.SelectedItem;
+            Resources.Culture = new CultureInfo(selected.Key);
+
+            // リソースファイルからラベル文字列を取得
+            label1.Text = Resources.Greeting;
+        }
+    }
+}
